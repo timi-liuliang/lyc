6b09f5c3c8096f0eb70663f43908595f66149168
Captain <qq79402005@gmail.com>
2023-07-16 16:53:43 +0800

    门铃

--- arduino/smart_home/music_door_bell/music_door_bell.ino
+++ arduino/smart_home/music_door_bell/music_door_bell.ino
@@ -1,11 +1,26 @@
+#include "Buzzer.h"
+#include "Sounds.h"
+#include "PH20Port.h"
 
+int touch_PIN2 = 2; 
+PH20Port buzzerplay(P9);
+Buzzer mBuzzer = Buzzer(buzzerplay.pin1());
+Buzzer buzzer(buzzerplay.pin1());
 
 void setup() 
 {
-  
+	pinMode(touch_PIN2,INPUT);             
 }
 
 void loop() 
 {
-
-}
+	if (digitalRead(touch_PIN2)==HIGH) 
+	{ 
+		mBuzzer.bendTones(1500, 2500, 1.05, 20, 8);
+		mBuzzer.bendTones(2499, 1500, 1.05, 25, 8);
+	}
+	else 
+	{
+		buzzer.noTone();
+  	}
+ }
